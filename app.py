f7b98d2fb49a65d2a0c144a680032ad19f6cb11a
gowthamsaib <gowthamsaib369@gmail.com>
2025-01-09 09:09:00 -0500

    fast api with catboost

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
 app = Flask(__name__)
 
-# Load model & scaler
-with open("house_model.pkl", "rb") as f:
-    model = pickle.load(f)
-
-with open("scaler.pkl", "rb") as f:
-    scaler = pickle.load(f)
+model = pickle.load(open("housepred.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route("/")
 def home():
-    return "House Price Prediction API is running"
+    return render_template("index.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.get_json()
-    features = np.array(data["features"]).reshape(1, -1)
-    features_scaled = scaler.transform(features)
-    prediction = model.predict(features_scaled)
+    features = [float(x) for x in request.form.values()]
+    final_features = np.array(features).reshape(1, -1)
+    final_features = scaler.transform(final_features)
+    prediction = model.predict(final_features)
 
-    return jsonify({
-        "predicted_price": round(float(prediction[0]), 2)
-    })
+    return render_template(
+        "index.html",
+        prediction_text=f"Predicted House Price: ${round(prediction[0], 2)}"
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
